8815870834b0d37376d48c12b4d54c71b5dd719e
SolbiShin <sbshin.dev@gmail.com>
2022-04-23 19:27:52 +0900

    api 목록 수정

--- store/photoStore.ts
+++ store/photoStore.ts
@@ -4,15 +4,23 @@ const key = 'XEgV7P-yEIq_ecleGiQHHFm-vosbWlyyEmp0cTntS04';
 
 export default class photoStore {
   // 최신 사진 목록
-  static getPhotoList() {
-    axios.get(`${baseURL}/photos/?client_id=${key}`).then((result) => {
+  static getPhotoList(): any {
+    return axios.get(`${baseURL}/photos/?client_id=${key}`).then((result) => {
       return result.data;
     });
   }
   // 사진 검색
-  static searchPhoto(query?: string) {
-    axios
+  static searchPhoto(query?: string): any {
+    return axios
       .get(`${baseURL}/search/photos/?client_id=${key}&query=${query}`)
+      .then((result) => {
+        return result.data;
+      });
+  }
+  // related Collection
+  static getCollectionList(id?: string): any {
+    return axios
+      .get(`${baseURL}/search/collections/?client_id=${key}&id=${id}`)
       .then((result) => {
         return result.data;
       });
